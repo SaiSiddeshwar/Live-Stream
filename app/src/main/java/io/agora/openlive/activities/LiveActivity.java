ca6b20c58ec72cca8ebf8969920a1c1c98c96daa
SaiSiddeshwar <sska@deakin.edu.au>
2020-07-04 03:36:43 +1000

    First Commit

---
+++ app/src/main/java/io/agora/openlive/activities/LiveActivity.java
@@ -0,0 +1,341 @@
+package io.agora.openlive.activities;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import io.agora.openlive.Message.InChannelMessageListAdapter;
+import io.agora.openlive.Message.Message;
+import io.agora.openlive.Message.MessageListDecoration;
+import io.agora.openlive.R;
+import io.agora.openlive.utils.LocalStatsData;
+import io.agora.openlive.utils.RemoteStatsData;
+import io.agora.openlive.utils.StatsData;
+import io.agora.openlive.utils.VideoGridContainer;
+import io.agora.rtc.Constants;
+import io.agora.rtc.IRtcEngineEventHandler;
+import io.agora.rtc.video.VideoEncoderConfiguration;
+
+public class LiveActivity extends RtcBaseActivity {
+    private static final String TAG = LiveActivity.class.getSimpleName();
+
+    private VideoGridContainer mVideoGridContainer;
+
+    private VideoEncoderConfiguration.VideoDimensions mVideoDimension;
+
+    private static final int CALL_OPTIONS_REQUEST = 3222;
+    private ArrayList<Message> mMsgList;
+    private InChannelMessageListAdapter mMsgAdapter;
+    private ImageView sendBtn;
+    private boolean IsMessageAvilable = false;
+    long last_text_edit = 0;
+    private EditText chatText;
+    public static boolean DEBUG_INFO_ENABLED = true; // Show debug/log info on screen
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_room);
+        initUI();
+        initData();
+        initMessageList();
+        notifyMessageChanged(new Message(new Message.User(0, null), "start join " + "channel" + " as " + (config().mUid & 0xFFFFFFFFL)));
+        sendBtn = findViewById(R.id.sent_message);
+        chatText = findViewById(R.id.chat_text);
+        message();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CALL_OPTIONS_REQUEST) {
+            RecyclerView msgListView = (RecyclerView) findViewById(R.id.msg_list);
+            msgListView.setVisibility(DEBUG_INFO_ENABLED ? View.VISIBLE : View.INVISIBLE);
+        }
+    }
+
+    public void message(){
+        chatText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().length() > 0) {
+                    IsMessageAvilable = true;
+                    sendBtn.setColorFilter(ContextCompat.getColor(LiveActivity.this, R.color.whit), PorterDuff.Mode.SRC_IN);
+
+                    last_text_edit = System.currentTimeMillis();
+//                    handler.postDelayed(input_finish_checker, delay);
+                } else {
+
+                    IsMessageAvilable = false;
+                    sendBtn.setColorFilter(ContextCompat.getColor(LiveActivity.this, R.color.white_500), PorterDuff.Mode.SRC_IN);
+
+                }
+            }
+        });
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (IsMessageAvilable || chatText.getText().toString().trim().length() > 0) {
+                    notifyMessageChanged(new Message(new Message.User(0, null),  chatText.getText().toString()));
+                    mMsgAdapter.notifyItemInserted(0);
+
+                }else {
+                    Toast.makeText(LiveActivity.this, "Type something before send",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+
+    }
+
+
+
+    private void initMessageList() {
+        mMsgList = new ArrayList<>();
+        RecyclerView msgListView = (RecyclerView) findViewById(R.id.msg_list);
+        mMsgAdapter = new InChannelMessageListAdapter(this, mMsgList);
+        mMsgAdapter.setHasStableIds(true);
+        msgListView.setAdapter(mMsgAdapter);
+        msgListView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), RecyclerView.VERTICAL, false));
+        msgListView.addItemDecoration(new MessageListDecoration());
+    }
+
+    private void notifyMessageChanged(Message msg) {
+        mMsgList.add(msg);
+        int MAX_MESSAGE_COUNT = 16;
+        if (mMsgList.size() > MAX_MESSAGE_COUNT) {
+            int toRemove = mMsgList.size() - MAX_MESSAGE_COUNT;
+            for (int i = 0; i < toRemove; i++) {
+                mMsgList.remove(i);
+            }
+        }
+
+        mMsgAdapter.notifyDataSetChanged();
+    }
+
+
+    private void initUI() {
+        TextView roomName = findViewById(R.id.live_room_name);
+        roomName.setText(config().getChannelName());
+        roomName.setSelected(true);
+        
+        initUserIcon();
+
+        int role = getIntent().getIntExtra(
+                io.agora.openlive.utils.Constants.KEY_CLIENT_ROLE,
+                Constants.CLIENT_ROLE_AUDIENCE);
+        boolean isBroadcaster =  (role == Constants.CLIENT_ROLE_BROADCASTER);
+
+
+        ImageView beautyBtn = findViewById(R.id.live_btn_beautification);
+        beautyBtn.setActivated(true);
+        rtcEngine().setBeautyEffectOptions(beautyBtn.isActivated(),
+                io.agora.openlive.utils.Constants.DEFAULT_BEAUTY_OPTIONS);
+
+        mVideoGridContainer = findViewById(R.id.live_video_grid_layout);
+        mVideoGridContainer.setStatsManager(statsManager());
+
+        rtcEngine().setClientRole(role);
+        if (isBroadcaster) startBroadcast();
+    }
+
+    private void initUserIcon() {
+        Bitmap origin = BitmapFactory.decodeResource(getResources(), R.drawable.fake_user_icon);
+        RoundedBitmapDrawable drawable = RoundedBitmapDrawableFactory.create(getResources(), origin);
+        drawable.setCircular(true);
+        ImageView iconView = findViewById(R.id.live_name_board_icon);
+        iconView.setImageDrawable(drawable);
+    }
+
+    private void initData() {
+        mVideoDimension = VideoEncoderConfiguration.VD_640x360;
+    }
+
+    @Override
+    protected void onGlobalLayoutCompleted() {
+        RelativeLayout topLayout = findViewById(R.id.live_room_top_layout);
+        RelativeLayout.LayoutParams params =
+                (RelativeLayout.LayoutParams) topLayout.getLayoutParams();
+        params.height = mStatusBarHeight + topLayout.getMeasuredHeight();
+        topLayout.setLayoutParams(params);
+        topLayout.setPadding(0, mStatusBarHeight, 0, 0);
+    }
+
+    private void startBroadcast() {
+        rtcEngine().setClientRole(Constants.CLIENT_ROLE_BROADCASTER);
+        SurfaceView surface = prepareRtcVideo(0, true);
+        mVideoGridContainer.addUserVideoSurface(0, surface, true);
+
+    }
+
+
+    @Override
+    public void onJoinChannelSuccess(String channel, int uid, int elapsed) {
+        // Do nothing at the moment
+    }
+
+    @Override
+    public void onUserJoined(int uid, int elapsed) {
+        // Do nothing at the moment
+    }
+
+    @Override
+    public void onUserOffline(final int uid, int reason) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                removeRemoteUser(uid);
+            }
+        });
+    }
+
+    @Override
+    public void onFirstRemoteVideoDecoded(final int uid, int width, int height, int elapsed) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                renderRemoteUser(uid);
+            }
+        });
+    }
+
+    private void renderRemoteUser(int uid) {
+        SurfaceView surface = prepareRtcVideo(uid, false);
+        mVideoGridContainer.addUserVideoSurface(uid, surface, false);
+    }
+
+    private void removeRemoteUser(int uid) {
+        removeRtcVideo(uid, false);
+        mVideoGridContainer.removeUserVideo(uid, false);
+    }
+
+    @Override
+    public void onLocalVideoStats(IRtcEngineEventHandler.LocalVideoStats stats) {
+        if (!statsManager().isEnabled()) return;
+
+        LocalStatsData data = (LocalStatsData) statsManager().getStatsData(0);
+        if (data == null) return;
+
+        data.setWidth(mVideoDimension.width);
+        data.setHeight(mVideoDimension.height);
+        data.setFramerate(stats.sentFrameRate);
+    }
+
+    @Override
+    public void onRtcStats(IRtcEngineEventHandler.RtcStats stats) {
+        if (!statsManager().isEnabled()) return;
+
+        LocalStatsData data = (LocalStatsData) statsManager().getStatsData(0);
+        if (data == null) return;
+
+        data.setLastMileDelay(stats.lastmileDelay);
+        data.setVideoSendBitrate(stats.txVideoKBitRate);
+        data.setVideoRecvBitrate(stats.rxVideoKBitRate);
+        data.setAudioSendBitrate(stats.txAudioKBitRate);
+        data.setAudioRecvBitrate(stats.rxAudioKBitRate);
+        data.setCpuApp(stats.cpuAppUsage);
+        data.setCpuTotal(stats.cpuAppUsage);
+        data.setSendLoss(stats.txPacketLossRate);
+        data.setRecvLoss(stats.rxPacketLossRate);
+    }
+
+    @Override
+    public void onNetworkQuality(int uid, int txQuality, int rxQuality) {
+        if (!statsManager().isEnabled()) return;
+
+        StatsData data = statsManager().getStatsData(uid);
+        if (data == null) return;
+
+        data.setSendQuality(statsManager().qualityToString(txQuality));
+        data.setRecvQuality(statsManager().qualityToString(rxQuality));
+    }
+
+    @Override
+    public void onRemoteVideoStats(IRtcEngineEventHandler.RemoteVideoStats stats) {
+        if (!statsManager().isEnabled()) return;
+
+        RemoteStatsData data = (RemoteStatsData) statsManager().getStatsData(stats.uid);
+        if (data == null) return;
+
+        data.setWidth(stats.width);
+        data.setHeight(stats.height);
+        data.setFramerate(stats.rendererOutputFrameRate);
+        data.setVideoDelay(stats.delay);
+    }
+
+    @Override
+    public void onRemoteAudioStats(IRtcEngineEventHandler.RemoteAudioStats stats) {
+        if (!statsManager().isEnabled()) return;
+
+        RemoteStatsData data = (RemoteStatsData) statsManager().getStatsData(stats.uid);
+        if (data == null) return;
+
+        data.setAudioNetDelay(stats.networkTransportDelay);
+        data.setAudioNetJitter(stats.jitterBufferDelay);
+        data.setAudioLoss(stats.audioLossRate);
+        data.setAudioQuality(statsManager().qualityToString(stats.quality));
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        statsManager().clearAllData();
+    }
+
+    public void onLeaveClicked(View view) {
+        finish();
+    }
+
+    public void onSwitchCameraClicked(View view) {
+        rtcEngine().switchCamera();
+    }
+
+    public void onBeautyClicked(View view) {
+        view.setActivated(!view.isActivated());
+        rtcEngine().setBeautyEffectOptions(view.isActivated(),
+                io.agora.openlive.utils.Constants.DEFAULT_BEAUTY_OPTIONS);
+    }
+
+    public void onMoreClicked(View view) {
+        // Do nothing at the moment
+    }
+
+    public void onPushStreamClicked(View view) {
+        // Do nothing at the moment
+    }
+
+
+}
